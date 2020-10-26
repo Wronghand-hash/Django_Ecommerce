13f09536e4f4d48f6172dc9fbc668524c3fe5557
Wronghand-hash <62405360+Wronghand-hash@users.noreply.github.com>
2020-10-26 10:31:05 +0330

    cart functionality added
    add and remove and some other things

--- store/urls.py
+++ store/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path('', views.frontpage, name="frontpage"),
     path('cart/', views.cart, name="cart"),
     path('store/', views.store , name="store"),
+    path('update_item/', views.updateItem , name="updateItem"),
     path('checkout/', views.checkout, name="checkout"),
     path('<slug:category_slug>/<slug:slug>', views.product_detail, name="product_detail"),
     path('<slug:slug>', views.category_detail, name="category_detail"),
+
+    
 ]
