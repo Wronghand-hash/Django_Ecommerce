735ff8c04f45ee43d2ca8b6e7cec56ee51917abe
comanchechao <61477751+comanchechao@users.noreply.github.com>
2020-10-31 01:48:46 +0330

    Revert "Merge branch 'master' into chao-development"
    This reverts commit 72511dc6acd0c5dc669f1f6f3483b2abb4ba4063, reversing
    changes made to da34a7de4566c05479e3d91f7dc7ee0c7a34fc78.

--- store/views.py
+++ store/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
-import json
-
 from .models import *
 from rest_framework.decorators import api_view
 from django.http import HttpRequest
@@ -47,19 +44,6 @@ def category_detail(request, slug):
 
     return render(request, 'store/category_detail.html', context)
 def store(request):
-
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order , created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
-        cartItems = order['get_cart_items']
-
-
-
     products = Product.objects.all()
 
     context = {
@@ -77,49 +61,10 @@ def cart(request):
 
 
     context = {
-        'items':items,
-        'order':order
+        'items':items
     }
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order , created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-    else:
-        items = []
-
-
-    context = {
-        'items':items,
-        'order':order
-    }
+    context = {}
     return render(request, 'store/checkout.html', context)
-
-
-def updateItem(request):
-    data = json.loads(request.body)
-    productId = data['productId']
-    action = data['action']
-
-    print('action:', action)
-    print('productId:', productId)
-
-    customer = request.user.customer
-    product = Product.objects.get(id=productId)
-    order , created = Order.objects.get_or_create(customer=customer, complete=False)
-
-    orderItem , created = OrderItem.objects.get_or_create(order=order , product=product)
-
-    if action == 'add':
-        orderItem.quantity = (orderItem.quantity + 1)
-    elif action == 'remove':
-        orderItem.quantity = (orderItem.quantity - 1)
-
-    orderItem.save()
-
-    if orderItem.quantity <= 0:
-        orderItem.delete()
-
-    return JsonResponse('item added to cart', safe=False
