e4b77850ee6d95f50b6dae6bfe1bba75147337e0
comanchechao <61477751+comanchechao@users.noreply.github.com>
2020-11-10 20:40:02 +0330

    Create Footer.vue

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-footer color="#00AE3A" shaped>
+    <v-row justify="center" no-gutters>
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="#fec601"
+        rounded
+        class="my-2 mx-1 black--text"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col class="py-4 text-center" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Acupulco Design</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+
+<script>
+export default {
+  data: () => ({
+    links: ["Home", "About Us", "Team", "Services", "Blog", "Contact Us"]
+  })
+};
+</script>
+
